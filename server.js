609ffdabbe13a8064bdacd0a140fcfe7d221fa36
Charley Wright <charleywright06@gmail.com>
2021-06-02 19:31:22 +0100

    Self host

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const httpProxy = require("http-proxy");
+const app = express();
+const proxy = httpProxy.createProxyServer();
+
+proxy.on("proxyReq", function (proxyReq) {
+  for (const header in proxyReq.getHeaders()) proxyReq.setHeader(header, "");
+  proxyReq.setHeader("host", proxyReq.host);
+});
+
+app.use("/", express.static("dist/"));
+app.all("/proxy(/*)", (req, res) => {
+  req.url = req.url.replace("/proxy", "");
+  proxy.web(req, res, { target: "https://kahoot.it" });
+});
+
+app.listen(8080, () => console.log("Listening on 8080"));
